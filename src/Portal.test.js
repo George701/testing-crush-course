c40d27f3a385fbd589d70d454d09479d696e916e
George701 <chernikov.georgii@gmail.com>
2022-09-21 16:22:41 +0300

    Add Portal and tests

---
+++ src/Portal.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Modal from './Portal'
+
+
+
+describe("Portal", () => {
+  const handleClose = jest.fn();
+  const testId = 'my-portal';
+
+  it('modal shows the children and a close button', () => {
+    render(
+      <Modal onClose={handleClose}>
+        <div data-testid={testId}>My Portal</div>
+      </Modal>
+    );
+
+    expect(screen.getByTestId(testId)).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button"));
+    expect(handleClose).toBeCalledTimes(1);
+  });
+
+  it('should be unmounted', () => {
+    const { unmount } = render(
+      <Modal>
+        <div data-testid={testId}>My Portal</div>
+      </Modal>
+    );
+
+    expect(screen.getByTestId(testId)).toBeInTheDocument();
+    unmount();
+    expect(screen.queryByTestId(testId)).not.toBeInTheDocument();
+  });
+});
